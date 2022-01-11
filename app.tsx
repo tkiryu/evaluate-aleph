443568dd7f82f3d449abe238d437ba80d2c688f6
tkiryu <tatsushi_kiryu@caddi.jp>
2022-01-11 23:14:32 +0900

    feat: add chakra-ui

--- app.tsx
+++ app.tsx
@@ -1,5 +1,7 @@
+// @deno-types="https://esm.sh/v58/@types/react/jsx-runtime.d.ts"
 import React, { FC } from 'react';
 import { createClient, Provider } from 'urql';
+import { ChakraProvider } from 'chakra-ui';
 
 const client = createClient({
   url: 'https://api.spacex.land/graphql/',
@@ -9,12 +11,14 @@ const client = createClient({
 export default function App({ Page, pageProps }: { Page: FC; pageProps: Record<string, unknown> }) {
   return (
     <Provider value={client}>
-      <main>
-        <head>
-          <meta name="viewport" content="width=device-width" />
-        </head>
-        <Page {...pageProps} />
-      </main>
+      <ChakraProvider>
+        <main>
+          <head>
+            <meta name="viewport" content="width=device-width" />
+          </head>
+          <Page {...pageProps} />
+        </main>
+      </ChakraProvider>
     </Provider>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,16 @@
 import React, { Suspense } from 'react';
+import { Center, CircularProgress } from 'chakra-ui';
 import { SpaceX } from '../components/SpaceX.tsx';
 
 export default function Home() {
   return (
-    <Suspense fallback={<p>loading...</p>}>
+    <Suspense
+      fallback={
+        <Center h="100vh">
+          <CircularProgress isIndeterminate />
+        </Center>
+      }
+    >
       <SpaceX />
     </Suspense>
   );
